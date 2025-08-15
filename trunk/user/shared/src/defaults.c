d01b7c2b726f89b7288b50f4d5b180c7cc3b73d2
h70076333 <h70076222@88.com>
2025-08-16 01:21:26 +0800

    Update defaults.c

--- trunk/user/shared/src/defaults.c
+++ trunk/user/shared/src/defaults.c
@@ -822,6 +822,10 @@ struct nvram_pair router_defaults[] = {
 	{ "zerotier_nat", "1" },
 	{ "zerotier_secret", "" },
 	{ "zero_staticnum_x", "0" },
+	{ "zero_afykey", "" },
+	{ "zero_afynen", "" },
+	{ "afycx_enable", "0" },
+	{ "gecoac_enable", "0" },
 #endif
 
 #if defined(APP_HXCLI)
